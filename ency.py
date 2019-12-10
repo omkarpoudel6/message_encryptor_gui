f92034cdd399d5b165ceddfa9980463d88d93a97
omkarpoudel6 <omkar.poudel77@gmail.com>
2019-12-10 16:08:03 +0545

    added

---
+++ ency.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+root=Tk()
+root.geometry("600x500")
+root.config(background="black")
+
+lbl1=Label(root,text=" ",background="black")
+lbl1.pack(fill=X)
+
+lbl2=Label(root,text=" ",background="black")
+lbl2.pack(fill=X)
+
+lbl3=Label(root,text=" ",background="black")
+lbl3.pack(fill=X)
+
+lbl4=Label(root,text=" ",background="black")
+lbl4.pack(fill=X)
+
+lbl5=Label(root,text=" ",background="black")
+lbl5.pack(fill=X)
+
+lbl6=Label(root,text=" ",background="black")
+lbl6.pack(fill=X)
+
+
+root.mainloop()
